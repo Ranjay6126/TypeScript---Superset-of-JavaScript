dbd737f1923bba4e053981f8687aa53c2ab83a18
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-03 21:51:31 +0530

    asynce await

--- Generics.ts
+++ Generics.ts
@@ -4,3 +4,26 @@ function getValue<T>(value: T): T {
 
 console.log(getValue<string>("Hello"));
 console.log(getValue<number>(100));
+
+//Type Assertions
+
+
+let value: any = "Hello";
+
+let length = (value as string).length;
+
+
+// Utility Types
+
+interface User {
+    name: string;
+    age: number;
+}
+
+type UpdateUser = Partial<User>;
+
+// Async await with typescript
+
+async function fetchData(): Promise<string> {
+    return "Data Received";
+}
