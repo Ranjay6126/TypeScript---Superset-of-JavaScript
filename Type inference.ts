436fdc755802723cf113f4c062ea797de4eb3e3f
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-06-03 21:40:32 +0530

    array , union types

---
+++ Type inference.ts
@@ -0,0 +1,17 @@
+//2. Type Inference
+
+let city = "Bangalore"; // string
+let salary = 50000;     // number
+
+// 3. Arrays and Tuples
+
+let nums: number[] = [1, 2, 3];
+
+let employee: [number, string] = [101, "Dhanush"];
+
+//Union Types 
+
+let id: number | string;
+
+id = 101;
+id = "EMP101";
